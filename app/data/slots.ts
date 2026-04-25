38cebe0a4cb038964eebd973619ae9616b2bf773
Maryam Fatima <maryamfatima6625@gmail.com>
2026-04-25 18:07:31 +0500

    update

--- app/data/slots.ts
+++ app/data/slots.ts
@@ -1,7 +1,7 @@
 export const SLOTS = [
-  { id: 1, time: "Saturday 25th April 08:30 AM", quota: 4 },
-  { id: 2, time: "Saturday 25th April 10:00 AM", quota: 4 },
-  { id: 3, time: "Saturday 25th April 12:00 PM", quota: 4 },
-  { id: 4, time: "Saturday 25th April 02:00 PM", quota: 5 },
-  { id: 5, time: "Saturday 25th April 04:00 PM", quota: 5 },
+  { id: 1, time: "Sunday 26th April 09:00 AM", quota: 4 },
+  { id: 2, time: "Sunday 26th April 10:00 AM", quota: 4 },
+  { id: 3, time: "Sunday 26th April 12:00 PM", quota: 4 },
+  { id: 4, time: "Sunday 26th April 02:00 PM", quota: 5 },
+  { id: 5, time: "Sunday 26th April 04:00 PM", quota: 5 },
 ];
