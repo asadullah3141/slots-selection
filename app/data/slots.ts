e39299c627aa6df032bbca33cc88d5be8f21c544
Maryam Fatima <maryamfatima6625@gmail.com>
2026-05-01 20:17:35 +0500

    update

--- app/data/slots.ts
+++ app/data/slots.ts
@@ -1,5 +1,5 @@
 export const SLOTS = [
-  { id: 1, time: "Saturday 2nd May 09:00 AM", quota: 2 },
+  { id: 1, time: "Saturday 2nd May 09:00 AM", quota: 3 },
   { id: 2, time: "Saturday 2nd May 10:00 AM", quota: 5 },
   { id: 3, time: "Saturday 2nd May 12:00 PM", quota: 5 },
   { id: 4, time: "Saturday 2nd May 02:00 PM", quota: 5 },
